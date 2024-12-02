8f7bd115cb0c93a5bb6a613a2d433376bb18cd61
Armand Murenzi Murayire <mura0083@algonquinlive.com>
2024-12-01 23:00:05 -0500

    final class exercise

---
+++ lib/main.dart
@@ -0,0 +1,273 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+// import 'package'
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MainScreen(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        textTheme: Theme.of(context).textTheme.copyWith(
+              bodyLarge: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white),
+            ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List _screens = [
+    const HomePage(),
+    const DataPage(),
+    const ContactPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (pageIndex) {
+          setState(() {
+            _currentIndex = pageIndex;
+          });
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.data_usage), label: 'Data'),
+          NavigationDestination(
+            icon: Icon(Icons.contact_mail),
+            label: 'Contact',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/image/background.jpg'),
+              fit: BoxFit.cover,
+            )),
+          )),
+          Expanded(
+            child: Container(
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: Transform.rotate(
+                    angle: 0.3,
+                    child: Text(
+                      'Hello World',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DataPage extends StatefulWidget {
+  const DataPage({super.key});
+
+  @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  late Future<List<Product>> _data;
+
+  @override
+  void initState() {
+    super.initState();
+    _data = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<Product>>(
+          future: _data,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final data = snapshot.data!;
+              return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                        child: ListTile(
+                      leading: Image.network(data[index].thumbnail),
+                      title: Text(data[index].name),
+                      subtitle: Text(data[index].description),
+                    ));
+                  });
+            } else {
+              return const Text('No data');
+            }
+          }),
+    );
+  }
+
+  Future<List<Product>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://dummyjson.com/products'));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body)['products'];
+      // print(jsonData);
+      return jsonData
+          .map((data) => Product(
+                id: data['id'],
+                name: data['title'],
+                description: data['description'],
+                thumbnail: data['thumbnail'],
+              ))
+          .toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
+
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final String thumbnail;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.thumbnail,
+  });
+}
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? name, email, message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Contact Us')),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Need help? Send us a message',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8.0),
+                TextFormField(
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.person),
+                    hintText: 'Enter your name',
+                    labelText: 'Name',
+                  ),
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Please enter your name'
+                      : null,
+                  onSaved: (value) => name = value,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.email),
+                    hintText: 'Enter your email',
+                    labelText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Please enter your email'
+                      : null,
+                  onSaved: (value) => email = value,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.message),
+                    hintText: 'Enter your message',
+                    labelText: 'Message',
+                  ),
+                  keyboardType: TextInputType.multiline,
+                  textInputAction: TextInputAction.newline,
+                  maxLines: 5,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Please enter your message'
+                      : null,
+                  onSaved: (value) => message = value,
+                ),
+                const SizedBox(height: 16.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Submitting $name')));
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
